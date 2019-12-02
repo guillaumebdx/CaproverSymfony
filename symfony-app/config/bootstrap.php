92f6c6c5502b1049b182e1bbb1896b9d43771ae0
Guillaume HARARI <guillaumeharari@hotmail.com>
2019-12-02 23:22:49 +0100

    Restore file after dump die

--- symfony-app/config/bootstrap.php
+++ symfony-app/config/bootstrap.php
@@ -2,8 +2,6 @@
 
 use Symfony\Component\Dotenv\Dotenv;
 
-var_dump(scandir(dirname(__DIR__)));
-die;
 require dirname(__DIR__).'/vendor/autoload.php';
 
 // Load cached env vars if the .env.local.php file exists
